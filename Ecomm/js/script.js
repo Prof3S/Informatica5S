7d5aedfffaa3a370126054ca545d452b064e0566
Prof3S <maifredi.riccardo@istitutomoreschi.edu.it>
2024-06-02 23:26:53 +0200

    Last commits

--- Ecomm/js/script.js
+++ Ecomm/js/script.js
@@ -1,9 +1,5 @@
 // script.js
 
-function openNewWindow() {
-    window.open('register.html', '_blank', 'width=800,height=600');
-}
-
-function opencart() {
-    window.location.href = 'cart.html';
+function openNewWindow(url) {
+    window.open(url, '_blank', 'width=800,height=600');
 }
